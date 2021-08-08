b16a6a83b86eb2a2c37a8ab52485e356d244f9c1
Andrei Neviarovskiy <never111194@gmail.com>
2021-08-08 20:07:43 +0300

    Initial Commit

---
+++ HomeWork5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork5
+//
+//  Created by Андрей on 8.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
